e89afa2410badf3ef7ca096814e16552bd123ffa
clocktower39 <clocktower39@aol.com>
2024-10-04 21:23:20 -0700

    Add background circles to icon and use dynamic colors from state

--- src/Components/CampComponent.jsx
+++ src/Components/CampComponent.jsx
@@ -1,15 +1,30 @@
 import React from "react";
-import { Button, Grid, Typography } from "@mui/material";
+import { Box, Button, Grid, Typography } from "@mui/material";
 
 export default function CampComponent({ camp, index }) {
   return (
     <Grid xs={12} sm={6} md={4} sx={{ margin: "50px 0" }}>
       <Grid container justifyContent="center">
-        {camp?.icon}
+        <Box
+          sx={{
+            backgroundColor: "#5F6574",
+            borderRadius: "50%",
+          }}
+        >
+          <Box
+            sx={{
+              backgroundColor: camp.color || '#000',
+              borderRadius: "50%",
+              margin: '5px'
+            }}
+          >
+            {camp?.icon}
+          </Box>
+        </Box>
       </Grid>
       <Grid container justifyContent="center">
         <Typography
-          color="#8e43ba"
+          color={camp.color || "#FFF"}
           variant="h5"
           textAlign="center"
           sx={{ fontFamily: "montserrat", padding: "15px 0", maxWidth: "250px" }}
