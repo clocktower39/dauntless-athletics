8ab659f0ea15234244927422b3ac8344c1891567
clocktower39 <clocktower39@aol.com>
2024-11-06 12:16:49 -0700

    udpate camps

--- src/states.jsx
+++ src/states.jsx
@@ -17,72 +17,17 @@ dayjs.extend(advancedFormat);
 
 const classes = {
   DauntlessClassesIcon: {
-    fontSize: { xs: '4em', md: '6em', },
-    padding: '15px',
+    fontSize: { xs: "4em", md: "6em" },
+    padding: "15px",
   },
   DauntlessCampIcon: {
     height: "160px",
     width: "160px",
     scale: 0.8,
   },
-}
+};
 
 export const camps = [
-  {
-    icon: <AirlineStopsIcon sx={{ ...classes.DauntlessCampIcon, }} />,
-    title: "Focused Tumbling Clinic Back Handspring",
-    color: "#8e43ba",
-    date: dayjs.utc(new Date("2024-10-19")).format("dddd, MMMM Do"),
-    time: "3:30 - 4:30 pm",
-    place: "Dauntless Athletics",
-    cost: 25,
-    buttonText: "Register For Clinic",
-    link: "https://app.iclasspro.com/portal/dauntlessathletics/camps/18",
-  },
-  {
-    icon: <StarsIcon sx={{ ...classes.DauntlessCampIcon, }} />,
-    title: "Focused Tumbling Clinic Back Tucks and Up",
-      color: "#8e43ba",
-    date: dayjs.utc(new Date("2024-10-19")).format("dddd, MMMM Do"),
-    time: "3:30 - 4:30 pm",
-    place: "Dauntless Athletics",
-    cost: 25,
-    buttonText: "Register For Clinic",
-    link: "https://app.iclasspro.com/portal/dauntlessathletics/camps/18",
-  },
-  {
-    icon: <AirlineStopsIcon sx={{ ...classes.DauntlessCampIcon, }} />,
-    title: "Focused Tumbling Clinic Back ",
-      color: "#8e43ba",
-    date: dayjs.utc(new Date("2024-10-19")).format("dddd, MMMM Do"),
-    time: "4:30 - 5:30 pm",
-    place: "Dauntless Athletics",
-    cost: 25,
-    buttonText: "Register For Clinic",
-    link: "https://app.iclasspro.com/portal/dauntlessathletics/camps/18",
-  },
-  {
-    icon: <SportsGymnasticsIcon sx={{ ...classes.DauntlessCampIcon, }} />,
-    title: "Focused Tumbling Clinic Aerial",
-      color: "#8e43ba",
-    date: dayjs.utc(new Date("2024-10-19")).format("dddd, MMMM Do"),
-    time: "5:30 - 6:30 pm",
-    place: "Dauntless Athletics",
-    cost: 25,
-    buttonText: "Register For Clinic",
-    link: "https://app.iclasspro.com/portal/dauntlessathletics/camps/18",
-  },
-  {
-    icon: <StarsIcon sx={{ ...classes.DauntlessCampIcon, }} />,
-    title: "October Tumbling Camp",
-      color: "#8e43ba",
-    date: dayjs.utc(new Date("2024-10-20")).format("dddd, MMMM Do"),
-    time: "1:00 - 4:00 pm",
-    place: "Dauntless Athletics",
-    cost: 69,
-    buttonText: "Register For Camp",
-    link: "https://app.iclasspro.com/portal/dauntlessathletics/camps/1",
-  },
   // {
   //   icon: <SportsGymnasticsIcon sx={{ ...classes.DauntlessCampIcon, }} />,
   //   title: "Aerial Intensive with Coach Dom",
@@ -95,9 +40,9 @@ export const camps = [
   //   link: "https://app.iclasspro.com/portal/dauntlessathletics/camps/19",
   // },
   {
-    icon: <AirlineStopsIcon sx={{ ...classes.DauntlessCampIcon, }} />,
+    icon: <AirlineStopsIcon sx={{ ...classes.DauntlessCampIcon }} />,
     title: "Focused Tumbling Clinic Back Handspring",
-      color: "#065E99",
+    color: "#065E99",
     date: dayjs.utc(new Date("2024-11-16")).format("dddd, MMMM Do"),
     time: "2:30 - 3:30 pm",
     place: "Dauntless Athletics",
@@ -106,9 +51,9 @@ export const camps = [
     link: "https://app.iclasspro.com/portal/dauntlessathletics/camps/18",
   },
   {
-    icon: <StarsIcon sx={{ ...classes.DauntlessCampIcon, }} />,
+    icon: <StarsIcon sx={{ ...classes.DauntlessCampIcon }} />,
     title: "Focused Tumbling Clinic Back Tucks and Up",
-      color: "#065E99",
+    color: "#065E99",
     date: dayjs.utc(new Date("2024-11-16")).format("dddd, MMMM Do"),
     time: "3:30 - 4:30 pm",
     place: "Dauntless Athletics",
@@ -117,9 +62,9 @@ export const camps = [
     link: "https://app.iclasspro.com/portal/dauntlessathletics/camps/18",
   },
   {
-    icon: <AirlineStopsIcon sx={{ ...classes.DauntlessCampIcon, }} />,
+    icon: <AirlineStopsIcon sx={{ ...classes.DauntlessCampIcon }} />,
     title: "Focused Tumbling Clinic Back Handspring",
-      color: "#065E99",
+    color: "#065E99",
     date: dayjs.utc(new Date("2024-11-16")).format("dddd, MMMM Do"),
     time: "4:30 - 5:30 pm",
     place: "Dauntless Athletics",
@@ -128,9 +73,9 @@ export const camps = [
     link: "https://app.iclasspro.com/portal/dauntlessathletics/camps/18",
   },
   {
-    icon: <SportsGymnasticsIcon sx={{ ...classes.DauntlessCampIcon, }} />,
+    icon: <SportsGymnasticsIcon sx={{ ...classes.DauntlessCampIcon }} />,
     title: "Focused Tumbling Clinic Aerial",
-      color: "#065E99",
+    color: "#065E99",
     date: dayjs.utc(new Date("2024-11-16")).format("dddd, MMMM Do"),
     time: "5:30 - 6:30 pm",
     place: "Dauntless Athletics",
@@ -139,9 +84,9 @@ export const camps = [
     link: "https://app.iclasspro.com/portal/dauntlessathletics/camps/18",
   },
   {
-    icon: <StarsIcon sx={{ ...classes.DauntlessCampIcon, }} />,
+    icon: <StarsIcon sx={{ ...classes.DauntlessCampIcon }} />,
     title: "November Tumbling Camp",
-      color: "#065E99",
+    color: "#065E99",
     date: dayjs.utc(new Date("2024-11-17")).format("dddd, MMMM Do"),
     time: "1:00 - 4:00 pm",
     place: "Dauntless Athletics",
@@ -150,9 +95,9 @@ export const camps = [
     link: "https://app.iclasspro.com/portal/dauntlessathletics/camps/1",
   },
   {
-    icon: <AirlineStopsIcon sx={{ ...classes.DauntlessCampIcon, }} />,
+    icon: <AirlineStopsIcon sx={{ ...classes.DauntlessCampIcon }} />,
     title: "Focused Tumbling Clinic Back Handspring",
-      color: "#f01313",
+    color: "#f01313",
     date: dayjs.utc(new Date("2024-12-14")).format("dddd, MMMM Do"),
     time: "2:30 - 3:30 pm",
     place: "Dauntless Athletics",
@@ -161,9 +106,9 @@ export const camps = [
     link: "https://app.iclasspro.com/portal/dauntlessathletics/camps/18",
   },
   {
-    icon: <StarsIcon sx={{ ...classes.DauntlessCampIcon, }} />,
+    icon: <StarsIcon sx={{ ...classes.DauntlessCampIcon }} />,
     title: "Focused Tumbling Clinic Back Tucks and Up",
-      color: "#f01313",
+    color: "#f01313",
     date: dayjs.utc(new Date("2024-12-14")).format("dddd, MMMM Do"),
     time: "3:30 - 4:30 pm",
     place: "Dauntless Athletics",
@@ -172,9 +117,9 @@ export const camps = [
     link: "https://app.iclasspro.com/portal/dauntlessathletics/camps/18",
   },
   {
-    icon: <AirlineStopsIcon sx={{ ...classes.DauntlessCampIcon, }} />,
+    icon: <AirlineStopsIcon sx={{ ...classes.DauntlessCampIcon }} />,
     title: "Focused Tumbling Clinic Back Handspring",
-      color: "#f01313",
+    color: "#f01313",
     date: dayjs.utc(new Date("2024-12-14")).format("dddd, MMMM Do"),
     time: "4:30 - 5:30 pm",
     place: "Dauntless Athletics",
@@ -183,9 +128,9 @@ export const camps = [
     link: "https://app.iclasspro.com/portal/dauntlessathletics/camps/18",
   },
   {
-    icon: <SportsGymnasticsIcon sx={{ ...classes.DauntlessCampIcon, }} />,
+    icon: <SportsGymnasticsIcon sx={{ ...classes.DauntlessCampIcon }} />,
     title: "Focused Tumbling Clinic Aerial",
-      color: "#f01313",
+    color: "#f01313",
     date: dayjs.utc(new Date("2024-12-14")).format("dddd, MMMM Do"),
     time: "5:30 - 6:30 pm",
     place: "Dauntless Athletics",
@@ -194,10 +139,65 @@ export const camps = [
     link: "https://app.iclasspro.com/portal/dauntlessathletics/camps/18",
   },
   {
-    icon: <StarsIcon sx={{ ...classes.DauntlessCampIcon, }} />,
-    title: "November Tumbling Camp",
-      color: "#f01313",
+    icon: <StarsIcon sx={{ ...classes.DauntlessCampIcon }} />,
+    title: "December Tumbling Camp",
+    color: "#f01313",
     date: dayjs.utc(new Date("2024-12-15")).format("dddd, MMMM Do"),
+    time: "1:00 - 4:00 pm",
+    place: "Dauntless Athletics",
+    cost: 69,
+    buttonText: "Register For Camp",
+    link: "https://app.iclasspro.com/portal/dauntlessathletics/camps/1",
+  },
+  {
+    icon: <AirlineStopsIcon sx={{ ...classes.DauntlessCampIcon }} />,
+    title: "Focused Tumbling Clinic Back Handspring",
+    color: "#8e43ba",
+    date: dayjs.utc(new Date("2025-01-25")).format("dddd, MMMM Do"),
+    time: "2:30 - 3:30 pm",
+    place: "Dauntless Athletics",
+    cost: 25,
+    buttonText: "Register For Clinic",
+    link: "https://app.iclasspro.com/portal/dauntlessathletics/camps/18",
+  },
+  {
+    icon: <StarsIcon sx={{ ...classes.DauntlessCampIcon }} />,
+    title: "Focused Tumbling Clinic Back Tucks and Up",
+    color: "#8e43ba",
+    date: dayjs.utc(new Date("2025-01-25")).format("dddd, MMMM Do"),
+    time: "3:30 - 4:30 pm",
+    place: "Dauntless Athletics",
+    cost: 25,
+    buttonText: "Register For Clinic",
+    link: "https://app.iclasspro.com/portal/dauntlessathletics/camps/18",
+  },
+  {
+    icon: <AirlineStopsIcon sx={{ ...classes.DauntlessCampIcon }} />,
+    title: "Focused Tumbling Clinic Back Handspring",
+    color: "#8e43ba",
+    date: dayjs.utc(new Date("2025-01-25")).format("dddd, MMMM Do"),
+    time: "4:30 - 5:30 pm",
+    place: "Dauntless Athletics",
+    cost: 25,
+    buttonText: "Register For Clinic",
+    link: "https://app.iclasspro.com/portal/dauntlessathletics/camps/18",
+  },
+  {
+    icon: <SportsGymnasticsIcon sx={{ ...classes.DauntlessCampIcon }} />,
+    title: "Focused Tumbling Clinic Aerial",
+    color: "#8e43ba",
+    date: dayjs.utc(new Date("2025-01-25")).format("dddd, MMMM Do"),
+    time: "5:30 - 6:30 pm",
+    place: "Dauntless Athletics",
+    cost: 25,
+    buttonText: "Register For Clinic",
+    link: "https://app.iclasspro.com/portal/dauntlessathletics/camps/18",
+  },
+  {
+    icon: <StarsIcon sx={{ ...classes.DauntlessCampIcon }} />,
+    title: "January Tumbling Camp",
+    color: "#8e43ba",
+    date: dayjs.utc(new Date("2025-01-26")).format("dddd, MMMM Do"),
     time: "1:00 - 4:00 pm",
     place: "Dauntless Athletics",
     cost: 69,
@@ -211,43 +211,49 @@ export const dauntlessClasses = [
     title: "Tumbling",
     description:
       "We offer every level of tumbling to teach kids basic to advanced tumbling skills. We teach solid fundamentals and spot the kids throughout the skills to ensure their safety and build their confidence. This allows them to acquire skills faster.",
-    icon: <AirlineStopsIcon sx={{ ...classes.DauntlessClassesIcon, color: 'rgb(0, 188, 212)', }} />,
+    icon: <AirlineStopsIcon sx={{ ...classes.DauntlessClassesIcon, color: "rgb(0, 188, 212)" }} />,
   },
   {
     title: "Tumbling Camp",
     description:
       "Let Dauntless Athletics get you ready for your cheer team final selection. Let us help you GET YOUR SKILLS!!",
-    icon: <SportsGymnasticsIcon sx={{ ...classes.DauntlessClassesIcon, color: 'rgb(244, 67, 54)', }} />,
+    icon: (
+      <SportsGymnasticsIcon sx={{ ...classes.DauntlessClassesIcon, color: "rgb(244, 67, 54)" }} />
+    ),
   },
   {
     title: "Strength, Conditioning & Flexibility",
     description:
       "We provide circuit style training to help improve the kids' muscular and cardiovascular endurance as well as proper technique for lifting to help build balance and strong muscles.",
-    icon: <FitnessCenterIcon sx={{ ...classes.DauntlessClassesIcon, color: 'rgb(255, 144, 105)', }} />,
+    icon: (
+      <FitnessCenterIcon sx={{ ...classes.DauntlessClassesIcon, color: "rgb(255, 144, 105)" }} />
+    ),
   },
   {
     title: "Twisting",
     description:
       "We offer an elite tumbling class for athletes who want to learn twisting, combo passes and double flipping skills. The athletes will be spotted repeatedly to help understand the high level skills they will be asked to do.",
-    icon: <StormIcon sx={{ ...classes.DauntlessClassesIcon, color: 'rgb(243, 199, 37)', }} />,
+    icon: <StormIcon sx={{ ...classes.DauntlessClassesIcon, color: "rgb(243, 199, 37)" }} />,
   },
   {
     title: "Stunting",
     description:
       "We offer stunting classes for building your skill and dexterity. Basic two-legged stunts, one-legged extended stunts, and high flying basket tosses are only a portion of what we offer.",
-    icon: <SportsKabaddiIcon sx={{ ...classes.DauntlessClassesIcon, color: 'rgb(243, 199, 37)', }} />,
+    icon: (
+      <SportsKabaddiIcon sx={{ ...classes.DauntlessClassesIcon, color: "rgb(243, 199, 37)" }} />
+    ),
   },
   {
     title: "Open Gym",
     description:
       "We offer Open Gym every Monday from 8:30 - 10:30 PM. Bring your friends and have a great time at Dauntless. (*High School and College)",
-    icon: <CropFreeIcon sx={{ ...classes.DauntlessClassesIcon, color: 'rgb(184, 255, 0)', }} />,
+    icon: <CropFreeIcon sx={{ ...classes.DauntlessClassesIcon, color: "rgb(184, 255, 0)" }} />,
   },
   {
     title: "High School Onsite Training",
     description:
       "We work with several high school teams across the Valley and offer tumbling on a non-spring tumbling floor. They get more tumbling reps on the same apparatus on which they will compete. Additionally, we use the spring floor to help them gain a better understanding of the skills they are learning. All these techniques help us provide them the most optimal tumbling training.",
-    icon: <SchoolIcon sx={{ ...classes.DauntlessClassesIcon, color: 'rgb(146, 237, 86)', }} />,
+    icon: <SchoolIcon sx={{ ...classes.DauntlessClassesIcon, color: "rgb(146, 237, 86)" }} />,
   },
 ];
 
@@ -257,7 +263,7 @@ export const pricingOptions = [
     cost: "$85",
     duration: "Per Month",
     buttonText: "Schedule Now",
-    buttonLink: 'https://www.iclassprov2.com/parentportal/dauntlessathletics/classes',
+    buttonLink: "https://www.iclassprov2.com/parentportal/dauntlessathletics/classes",
     backgroundColor: "#6391FF",
   },
   {
@@ -265,7 +271,7 @@ export const pricingOptions = [
     cost: "$110",
     duration: "Per Month",
     buttonText: "Schedule Now",
-    buttonLink: 'https://www.iclassprov2.com/parentportal/dauntlessathletics/classes',
+    buttonLink: "https://www.iclassprov2.com/parentportal/dauntlessathletics/classes",
     backgroundColor: "#B0DE79",
   },
   {
@@ -273,7 +279,7 @@ export const pricingOptions = [
     cost: "$130",
     duration: "Per Month",
     buttonText: "Schedule Now",
-    buttonLink: 'https://www.iclassprov2.com/parentportal/dauntlessathletics/classes',
+    buttonLink: "https://www.iclassprov2.com/parentportal/dauntlessathletics/classes",
     backgroundColor: "#FF5B93",
   },
   {
@@ -281,7 +287,7 @@ export const pricingOptions = [
     cost: "$140",
     duration: "Per Month",
     buttonText: "Schedule Now",
-    buttonLink: 'https://www.iclassprov2.com/parentportal/dauntlessathletics/classes',
+    buttonLink: "https://www.iclassprov2.com/parentportal/dauntlessathletics/classes",
     backgroundColor: "#01E2FE",
   },
   {
@@ -289,7 +295,7 @@ export const pricingOptions = [
     cost: "$150",
     duration: "Per Month",
     buttonText: "Schedule Now",
-    buttonLink: 'https://www.iclassprov2.com/parentportal/dauntlessathletics/classes',
+    buttonLink: "https://www.iclassprov2.com/parentportal/dauntlessathletics/classes",
     backgroundColor: "#6184D8",
   },
   {
@@ -297,7 +303,7 @@ export const pricingOptions = [
     cost: "$160",
     duration: "Per Month",
     buttonText: "Schedule Now",
-    buttonLink: 'https://www.iclassprov2.com/parentportal/dauntlessathletics/classes',
+    buttonLink: "https://www.iclassprov2.com/parentportal/dauntlessathletics/classes",
     backgroundColor: "#533A71",
   },
   {
@@ -305,7 +311,7 @@ export const pricingOptions = [
     cost: "$170",
     duration: "Per Month",
     buttonText: "Schedule Now",
-    buttonLink: 'https://www.iclassprov2.com/parentportal/dauntlessathletics/classes',
+    buttonLink: "https://www.iclassprov2.com/parentportal/dauntlessathletics/classes",
     backgroundColor: "#00BFA5",
   },
   {
@@ -313,7 +319,7 @@ export const pricingOptions = [
     cost: "$175",
     duration: "Per Month",
     buttonText: "Schedule Now",
-    buttonLink: 'https://www.iclassprov2.com/parentportal/dauntlessathletics/classes',
+    buttonLink: "https://www.iclassprov2.com/parentportal/dauntlessathletics/classes",
     backgroundColor: "#5D737E",
   },
   {
@@ -321,7 +327,7 @@ export const pricingOptions = [
     cost: "Varies",
     duration: "Call for Pricing",
     buttonText: "(480) 214-3908",
-    buttonLink: 'https://www.iclassprov2.com/parentportal/dauntlessathletics/classes',
+    buttonLink: "https://www.iclassprov2.com/parentportal/dauntlessathletics/classes",
     backgroundColor: "#86DD1C",
     optionalTextList: ["Prices vary by coach"],
   },
@@ -330,7 +336,7 @@ export const pricingOptions = [
     cost: "Varies",
     duration: "1 - 1.5 Hours",
     buttonText: "Schedule Now",
-    buttonLink: 'https://www.iclassprov2.com/parentportal/dauntlessathletics/classes',
+    buttonLink: "https://www.iclassprov2.com/parentportal/dauntlessathletics/classes",
     backgroundColor: "#7EE0BF",
     optionalTextList: ["$35 for 1 hour single class", "$45 for 1.5 hour single class"],
   },
@@ -339,7 +345,7 @@ export const pricingOptions = [
     cost: "$69",
     duration: "3 Hours",
     buttonText: "Schedule Now",
-    buttonLink: 'https://www.iclassprov2.com/parentportal/dauntlessathletics/camps?camptype=1',
+    buttonLink: "https://www.iclassprov2.com/parentportal/dauntlessathletics/camps?camptype=1",
     backgroundColor: "#83BCE9",
     optionalTextList: ["Beginner", "Intermediate", "Advanced", "*Price is per athlete"],
   },
