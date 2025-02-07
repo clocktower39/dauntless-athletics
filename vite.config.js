3972067dc219fef42df72858806daecba152976f
clocktower39 <clocktower39@aol.com>
2025-02-07 04:07:07 -0700

    upgrade packages, change some to devDependencies, configure rollup for chunking

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,46 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import PrerenderSPAPlugin from '@prerenderer/rollup-plugin';
-import PuppeteerRenderer from '@prerenderer/renderer-puppeteer';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import PrerenderSPAPlugin from "@prerenderer/rollup-plugin";
+import PuppeteerRenderer from "@prerenderer/renderer-puppeteer";
 
 export default defineConfig({
   plugins: [
     react(),
     PrerenderSPAPlugin({
-      staticDir: './dist',
-      routes: ['/','/camps','/college-combine','/class-schedule','/services','/staff','/facility','/contact-us'],
+      staticDir: "./dist",
+      routes: [
+        "/",
+        "/camps",
+        "/college-combine",
+        "/class-schedule",
+        "/services",
+        "/staff",
+        "/facility",
+        "/contact-us",
+      ],
       renderer: new PuppeteerRenderer({
         headless: true,
-        renderAfterDocumentEvent: 'render-event',
+        renderAfterDocumentEvent: "render-event",
       }),
     }),
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          "react-vendor": ["react", "react-dom"],
+          "mui-vendor": [
+            "@mui/material",
+            "@mui/icons-material",
+            "@mui/styles",
+            "@emotion/react",
+            "@emotion/styled",
+          ],
+          "date-utils": ["dayjs"],
+          utils: ["axios", "react-player"],
+          "router-vendor": ["react-router", "react-router-dom", "react-router-hash-link", ],
+        },
+      },
+    },
+  },
 });
