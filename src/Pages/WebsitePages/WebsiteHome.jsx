651b8514865e265cfbdd5127c5ae235dd95f8d82
clocktower39 <clocktower39@aol.com>
2025-06-30 14:54:01 -0700

    add google maps to home screen

--- src/Pages/WebsitePages/WebsiteHome.jsx
+++ src/Pages/WebsitePages/WebsiteHome.jsx
@@ -83,6 +83,18 @@ export default function Home() {
               padding: "25px 0px",
             }}
           >
+          <Grid container justifyContent="center">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3331.796753358821!2d-111.80131568449212!3d33.376371860489485!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x872ba8fa94329e07%3A0xf92b7042f9fabc0d!2sDauntless%20Athletics!5e0!3m2!1sen!2sus!4v1607308532613!5m2!1sen!2sus"
+              width="100%"
+              height="450"
+              frameborder="0"
+              style={{ border: 0 }}
+              allowfullscreen=""
+              aria-hidden="false"
+              tabindex="0"
+            ></iframe>
+          </Grid>
             <Grid container>
               <Grid container>
                 <Grid container justifyContent="center" size={12}>
